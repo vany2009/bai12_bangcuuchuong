cb42b30c472a6857e273d57996f7ec34067d8b50
MAY 06 <MAY 06@LAB2-20>
2024-11-18 10:25:57 +0700

    Add project files.

---
+++ bai12_bangcuuchuong.cpp
@@ -0,0 +1,13 @@
+// bai12_bangcuuchuong.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+using namespace std;
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int so = 2; so < 10; so++)
+			cout << so << "x" << i << "=" << so * i << "\n";
+	}
+	cout << endl;
+}
+
